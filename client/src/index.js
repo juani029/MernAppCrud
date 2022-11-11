08f6e2e051ab8917d2c713e79811a84fb80797ae
Juan Ignacio <93682709+juani029@users.noreply.github.com>
2022-11-11 17:52:23 -0300

    final upd

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,10 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
+import { config } from "dotenv";
 
-axios.defaults.baseURL = "http://localhost:3001";
+config();
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
